8ee298ad0175326955fa42d94977d70ba474afa9
Pablo San gregorio de lucas <psan-gre@c3r2s3.42madrid.com>
2020-07-23 12:11:58 +0200

    color.h norme ok.

--- includes/color.h
+++ includes/color.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   color.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: npinto-g <npinto-g@student.42.fr>          +#+  +:+       +#+        */
+/*   By: psan-gre <psan-gre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/17 17:38:53 by psan-gre          #+#    #+#             */
-/*   Updated: 2020/07/20 10:38:57 by npinto-g         ###   ########.fr       */
+/*   Updated: 2020/07/23 12:11:31 by psan-gre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,22 @@ typedef struct	s_color
 	unsigned char b;
 }				t_color;
 
-t_color	init_rgb(unsigned char r, unsigned char g, unsigned char b);
-int		ft_rgb_to_int(t_color rgb);
-t_color	ft_rgb_shade(t_color rgb, double shade_amount);
-t_color	ft_rgb_tint(t_color rgb, float tint_amount);
-t_color	ft_rgb_sum(t_color rgb1, t_color rgb2);
-t_color	ft_rgb_sum_ambient(t_color rgb1, t_color rgb2);
-t_color	ft_color_multiply(t_color rgb1, t_color rgb2);
-t_color	ft_color_addition(t_color rgb1, t_color rgb2);
-t_color	ft_color_dodge(t_color rgb1, t_color rgb2);
-t_color	ft_color_screen(t_color rgb1, t_color rgb2);
-t_color	ft_color_lighten(t_color rgb1, t_color rgb2);
-t_color	ft_color_merge(t_color rgb1, t_color rgb2);
-t_color	ft_color_overlay(t_color rgb1, t_color rgb2);
-t_color	ft_color_hardlight(t_color rgb1, t_color rgb2);
-t_color	ft_color_soflight(t_color rgb1, t_color rgb2);
-t_color ft_color_layer_opacity(t_color rgb, double alpha);
-void	ft_rgb_normalize(t_color *rgb);
+t_color			init_rgb(unsigned char r, unsigned char g, unsigned char b);
+int				ft_rgb_to_int(t_color rgb);
+t_color			ft_rgb_shade(t_color rgb, double shade_amount);
+t_color			ft_rgb_tint(t_color rgb, float tint_amount);
+t_color			ft_rgb_sum(t_color rgb1, t_color rgb2);
+t_color			ft_rgb_sum_ambient(t_color rgb1, t_color rgb2);
+t_color			ft_color_multiply(t_color rgb1, t_color rgb2);
+t_color			ft_color_addition(t_color rgb1, t_color rgb2);
+t_color			ft_color_dodge(t_color rgb1, t_color rgb2);
+t_color			ft_color_screen(t_color rgb1, t_color rgb2);
+t_color			ft_color_lighten(t_color rgb1, t_color rgb2);
+t_color			ft_color_merge(t_color rgb1, t_color rgb2);
+t_color			ft_color_overlay(t_color rgb1, t_color rgb2);
+t_color			ft_color_hardlight(t_color rgb1, t_color rgb2);
+t_color			ft_color_soflight(t_color rgb1, t_color rgb2);
+t_color			ft_color_layer_opacity(t_color rgb, double alpha);
+void			ft_rgb_normalize(t_color *rgb);
 
 #endif
