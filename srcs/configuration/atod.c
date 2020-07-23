d2fe51e23ece052941e5771b4a47df90984a9200
Pablo San gregorio de lucas <pablosan@tutanota.com>
2020-07-23 12:31:00 +0200

    atod.c norme ok.

--- srcs/configuration/atod.c
+++ srcs/configuration/atod.c
@@ -1,4 +1,16 @@
-#include "../../includes/minirt.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   atod.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psan-gre <psan-gre@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/23 12:28:43 by psan-gre          #+#    #+#             */
+/*   Updated: 2020/07/23 12:30:13 by psan-gre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
 
 size_t		declen(double d)
 {
@@ -15,11 +27,12 @@ size_t		declen(double d)
 		integer = d;
 		i++;
 	}
-	len = (d < 0)? len + 1 : len;
-	len += (i == 0)? 2 : i;
+	len = (d < 0) ? len + 1 : len;
+	len += (i == 0) ? 2 : i;
 	return (len);
 }
-long double ft_atod(const char *str)
+
+long double	ft_atod(const char *str)
 {
 	int				i;
 	long double		intg;
@@ -31,22 +44,19 @@ long double ft_atod(const char *str)
 	dec = 0;
 	sign = 1;
 	z_len = 0;
-	while(!(ft_isdigit(str[i])))
+	while (!(ft_isdigit(str[i])))
 	{
-		sign = (str[i] == '-') ? sign * -1: sign;
+		sign = (str[i] == '-') ? sign * -1 : sign;
 		i++;
 	}
 	intg = ft_atoi(str + i);
 	i += nbrlen(intg);
 	if (str[i] == '.')
 	{
-		i++;		
+		i++;
 		dec = ft_atoi(str + i);
-		while (str[i] == '0')
-		{
+		while (str[i++] == '0')
 			z_len++;
-			i++;
-		}
 	}
 	return (intg * sign + (dec / pow(10, nbrlen(dec) + z_len)) * sign);
 }
