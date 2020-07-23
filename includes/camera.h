054a907933c16f71d687ae966f5e99f263f934e9
Pablo San gregorio de lucas <psan-gre@c3r2s3.42madrid.com>
2020-07-23 12:08:53 +0200

    camera.h norme ok.

--- includes/camera.h
+++ includes/camera.h
@@ -6,7 +6,7 @@
 /*   By: psan-gre <psan-gre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/11 19:36:34 by psan-gre          #+#    #+#             */
-/*   Updated: 2020/07/23 12:04:53 by psan-gre         ###   ########.fr       */
+/*   Updated: 2020/07/23 12:08:32 by psan-gre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,17 @@
 
 typedef struct	s_screen
 {
-	t_vector pos;
-	t_vector x_axis;
-	t_vector y_axis;
-
-	int h;
-	int w;
+	t_vector	pos;
+	t_vector	x_axis;
+	t_vector	y_axis;
+	int			h;
+	int			w;
 }				t_screen;
 
 typedef struct	s_camera
 {
-	t_vector pos;
-
-	t_screen display;
-	double dist;
+	t_vector	pos;
+	t_screen	display;
+	double		dist;
 }				t_camera;
 #endif
