afaef42f26f88b1f5bb51403c7aace743e31cfde
pabgre <pablosan@tutanota.com>
2020-07-27 12:14:07 +0200

    Raytracer Utils 1 norme ok!

--- srcs/raytracer/raytracer_utils_1.c
+++ srcs/raytracer/raytracer_utils_1.c
@@ -6,7 +6,7 @@
 /*   By: psan-gre <psan-gre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/12 15:58:32 by psan-gre          #+#    #+#             */
-/*   Updated: 2020/07/27 12:06:49 by psan-gre         ###   ########.fr       */
+/*   Updated: 2020/07/27 12:13:16 by psan-gre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,35 +74,33 @@ t_ray_hit_data	hit_ray_in_any_object_lst(t_line ray, t_scene my_scene)
 	return (data);
 }
 
-t_ray_hit_data	light_info(t_scene scene, t_ray_hit_data hit_data)
+t_ray_hit_data	light_info(t_scene sc, t_ray_hit_data hit_d)
 {
 	float	fac;
 	t_list	*aux;
 	t_line	ray;
-	t_light	c_light;
-	t_color lightcolor;
+	t_light	light;
+	t_color l_c;
 
-	aux = scene.light_lst;
-	lightcolor = init_rgb(0, 0, 0);
+	aux = sc.light_lst;
+	l_c = init_rgb(0, 0, 0);
 	while (aux != NULL)
 	{
-		c_light = *((t_light*)aux->content);
-		ray.dir = normalize(subs(c_light.pos, hit_data.hit_point));
-		ray.point = add(hit_data.hit_point, prod(ray.dir, 0.000001));
+		light = *((t_light*)aux->content);
+		ray.dir = normalize(subs(light.pos, hit_d.hit_point));
+		ray.point = add(hit_d.hit_point, prod(ray.dir, 0.000001));
 		fac = 0;
-		if (!hit_ray_in_any_object_lst(ray, scene).hit_object || !which_is_near(
-			ray.point, c_light.pos, hit_ray_in_any_object_lst(ray, scene).hit_point))
-			fac = dot_prod(normalize(hit_data.normal), normalize(ray.dir));
-		fac = fac < 0 ? 0 : fac;
-		lightcolor = ft_rgb_sum(lightcolor,
-						ft_rgb_shade(c_light.color, fac * c_light.radius));
+		if (!hit_ray_in_any_object_lst(ray, sc).hit_object || !which_is_near(
+	ray.point, light.pos, hit_ray_in_any_object_lst(ray, sc).hit_point))
+			fac = dot_prod(normalize(hit_d.normal), normalize(ray.dir));
+		l_c = ft_rgb_sum(l_c,
+						ft_rgb_shade(light.color, fac * light.radius));
 		aux = aux->next;
 	}
-	lightcolor = ft_rgb_sum(lightcolor, ft_rgb_shade(scene.ambient.color, scene.ambient.radius));
-	hit_data.color = ft_color_multiply(hit_data.color, lightcolor);
-	lightcolor = ft_color_layer_opacity(lightcolor, 60);
-	hit_data.color = ft_color_screen(hit_data.color, lightcolor);
-	return (hit_data);
+	l_c = ft_rgb_sum(l_c, ft_rgb_shade(sc.ambient.color, sc.ambient.radius));
+	hit_d.color = ft_color_multiply(hit_d.color, l_c);
+	hit_d.color = ft_color_screen(hit_d.color, ft_color_layer_opacity(l_c, 60));
+	return (hit_d);
 }
 
 t_ray_hit_data	trace_ray(t_vector point, t_vector direction, t_scene my_scene)
